4124704148a459df5138b6d68c9e31855b5299dd
TemesgeA <temesgen.ayneka@gmail.com>
2022-09-22 12:00:05 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (<div className="App"><Calculator /></div>);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Calculator from './components/Calculator';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
 );
 
